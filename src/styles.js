767e4e9153359931b2603d03a61d2424d83ef980
moefaris <fadlandariss@gmail.com>
2024-11-08 20:05:44 +0700

    Last Pdding Update

--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,5 @@
 const styles = {
-  containerPadding: " pb-20 pt-20 p-4 md:px-20 relative overflow-hidden   ",
+  containerPadding: " pb-20 pt-20 p-8 md:px-20 relative overflow-hidden   ",
   colorGradient: "bg-gradient-to-tl from-primaryPurple via-darkenBlue to-primaryPurple"
   // bg-gradient-to-tl from-darkenBlue via-primaryPurple to-lightBlack
 };
